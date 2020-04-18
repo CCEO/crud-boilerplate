6127df663022d3c3c5f1066632cae10cbbd56f66
Julio García <julio.garcia@cceo.com.mx>
2020-04-18 09:55:49 -0500

    Was added new file for have the routes of the views

---
+++ routes/views.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Views Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register views routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
