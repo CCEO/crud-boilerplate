35a21fffcd1dccaba4cbcb44058a70c2942d533f
Julio García <julio.garcia@cceo.com.mx>
2020-04-18 10:26:57 -0500

    Was updated the structure for view and web routes

--- routes/views.php
+++ routes/views.php
@@ -10,3 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::view('/', 'welcome')->name('welcome');
+Route::view('/info', 'info')->name('info');
+
+Route::middleware(['auth', 'user.status'])->group(function () {
+    Route::view('system/users', 'system.users.index')->name('users.index');
+});
