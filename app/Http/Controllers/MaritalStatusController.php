cad3ef71eec4cdefd3defef63bd1a8898e5e453a
Julio García <julio.garcia@cceo.com.mx>
2020-04-21 09:28:01 -0500

    Was updated index function of marital status controller with new structure

--- app/Http/Controllers/MaritalStatusController.php
+++ app/Http/Controllers/MaritalStatusController.php
@@ -16,13 +16,42 @@ class MaritalStatusController extends Controller
     {
         $page = request()->get("page", false);
         $limit = request()->get("limit", false);
+        $orderBy = request()->get("orderBy", 'id');
+        $ascending = request()->get("ascending", 1);
         $filters = json_decode(request()->get("filters", "{}"), true);
+        $columns = json_decode(request()->get("columns", "[]"), true);
+
+        array_push($columns, 'id');
+        $appends = ['formatted_created_at', 'formatted_updated_at'];
 
         $maritalStatuss = MaritalStatus::where("id", ">", 0);
 
+        switch ($orderBy) {
+            case 'formatted_created_at':
+            case 'formatted_updated_at':
+                $maritalStatuss->orderBy($orderBy == 'formatted_created_at' ? 'created_at' : 'update_at', $ascending ? "ASC" : "DESC");
+                break;
+            default:
+                $maritalStatuss->orderBy($orderBy, $ascending ? "ASC" : "DESC");
+                break;
+        }
+
         foreach ($filters as $filter => $value) {
             if ($value && $filter != "reload")
-                $maritalStatuss->where($filter, $value);
+                switch ($filter) {
+                    case 'formatted_created_at':
+                    case 'formatted_updated_at':
+                        $filter = $filter == 'formatted_created_at' ? 'created_at' : 'update_at';
+                        $dates = explode(" a ", $value);
+                        if (count($dates) > 1)
+                            $maritalStatuss = $maritalStatuss->whereBetween(DB::raw('DATE(' . $filter . ')'), [$dates[0], $dates[1]]);
+                        else
+                            $maritalStatuss = $maritalStatuss->whereDate($filter, $dates[0]);
+                        break;
+                    default:
+                        $maritalStatuss->where($filter, 'LIKE', '%' . $value . '%');
+                        break;
+                }
         }
 
         $data = $maritalStatuss->get();
@@ -30,8 +59,9 @@ class MaritalStatusController extends Controller
         if ($limit && $page)
             $data = $data->slice(($page - 1) * $limit)->take($limit)->values();
 
-        $data = $data->map(function ($_data){
-            $_data = $_data->append('formatted_created_at', 'formatted_updated_at');
+        $data = $data->map(function ($_data) use ($columns, $appends) {
+            $_data = $_data->append($appends);
+            $_data = $_data->only($columns);
             return $_data;
         });
 
