0c7e3521e140ecce1054693071e5eea027292597
Julio García <julio.garcia@cceo.com.mx>
2020-04-22 18:28:50 -0500

    Was imported states table component to project

--- resources/js/components.js
+++ resources/js/components.js
@@ -1,7 +1,9 @@
 import UsersTable from "./components/users/UsersTable";
 import MaritalStatesTable from "./components/marital-states/MaritalStatesTable";
+import StatesTable from "./components/states/StatesTable";
 
 export default {
     UsersTable,
-    MaritalStatesTable
+    MaritalStatesTable,
+    StatesTable
 }
